042c06dbdeaf75147c9319ef2b62e42fea8c3c3c
Yuichi Takeuchi <uzuki05@takeyu-web.com>
2014-12-11 03:55:21 +0900

    実行時間指定なしの enqueue に対応

--- lib/sqs_adapter.rb
+++ lib/sqs_adapter.rb
@@ -3,16 +3,16 @@ require 'aws-sdk-core'
 class SqsAdapter
   class << self
     def enqueue(job)
-      raise job.serialize
+      sqs.send_message(
+          queue_url: get_queue_url(job),
+          message_body: MultiJson.dump(job.serialize)
+      )
     end
 
     def enqueue_at(job, timestamp)
       delay = timestamp.to_i - Time.current.to_i
-      resp = sqs.create_queue(
-          queue_name: job.queue_name,
-      )
       sqs.send_message(
-          queue_url: resp[:queue_url],
+          queue_url: get_queue_url(job),
           message_body: MultiJson.dump(job.serialize),
           delay_seconds: delay,
       )
@@ -20,6 +20,10 @@ class SqsAdapter
 
     def sqs
       Aws::SQS::Client.new
+    end
+
+    def get_queue_url(job)
+      sqs.create_queue(queue_name: job.queue_name)[:queue_url]
     end
   end
 
